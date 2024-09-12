1bc74bf158247bf4409010e1b10448974ddfd2bf
Joaco <joaco.kam@gmail.com>
2024-09-12 10:49:07 -0300

    Color orange in styles

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -17,7 +17,7 @@ export default function SideNav() {
 
   return (
     <div className="flex flex-col h-full px-3 py-4 md:px-2">
-      <div className="flex flex-row md:flex-col items-end md:items-center justify-end md:justify-center mb-2 p-4 rounded-md bg-blue-600 text-white">
+      <div className="flex flex-row md:flex-col items-end md:items-center justify-end md:justify-center mb-2 p-4 rounded-md bg-orange-400 text-white">
         <Link className="" href="/">
           <AcmeLogo />
         </Link>
@@ -25,7 +25,7 @@ export default function SideNav() {
           hola { userData.name }
         </div>
       </div>
-      <div className="smx:fixed smx:bottom-0 smx:left-0 smx:right-0 smx:p-3 smx:w-full smx:rounded-t-lg smx:bg-blue-600
+      <div className="smx:fixed smx:bottom-0 smx:left-0 smx:right-0 smx:p-3 smx:w-full smx:rounded-t-lg smx:bg-orange-400
                       flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
@@ -35,7 +35,7 @@ export default function SideNav() {
             await logout();
           }}
         >
-          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-orange-500 md:flex-none md:justify-start md:p-2 md:px-3">
             <PowerIcon className="w-6" />
             <div className="hidden md:block">Sign Out</div>
           </button>
