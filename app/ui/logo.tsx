f59b40b0ed6a45c6e8553a69eaebfef53933bd09
Joaco <joaco.kam@gmail.com>
2024-11-11 09:48:50 -0300

    Removed link on logo

--- app/ui/logo.tsx
+++ app/ui/logo.tsx
@@ -5,7 +5,7 @@ import { roboto } from '@/app/ui/fonts';
 export default function Logo() {
   return (
     <div className="p-5 bg-black text-white">
-      <Link href="/" className="flex flex-row justify-center items-center">
+      <Link href="" className="flex flex-row justify-center items-center">
         <Image src="/logo.png" width={60} height={60} alt="logo"/>
         <div className={`${roboto.className} bg-black text-white font-bold text-[29px] pl-5`}>Mr. Fitness Coach</div>
       </Link>
